7d3e43293bca29f4117745b915aa5e90fb982ca8
qollop <zevsergeo@gmail.com>
2025-03-10 00:01:03 -0300

    Внесены изменения в utils/check_pdf.py

--- utils/check_pdf.py
+++ utils/check_pdf.py
@@ -1,5 +1,5 @@
 from aiogram.types import Document
 
 def is_pdf(document: Document) -> bool:
-    """Проверяет, является ли загруженный файл PDF."""
+
     return document.mime_type == "application/pdf"
